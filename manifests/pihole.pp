d1c8d98653da681e2287f675462daf83dfd348c1
Dan Gordon <admin@dangordon.xyz>
2024-08-24 07:52:21 -0400

    Fix options for docker_network

--- manifests/pihole.pp
+++ manifests/pihole.pp
@@ -17,22 +17,14 @@ class rpi::pihole {
   }
 
   docker_network { 'pihole':
-    ensure            => present,
-    driver            => 'macvlan',
-    subnet            => '192.168.1.0/24',
-    gateway           => '192.168.1.1',
-    ip_range          => '192.168.1.17/32',
-    additional_flags  => ['-o parent=eth0'],
+    ensure   => present,
+    driver   => 'macvlan',
+    subnet   => '192.168.1.0/24',
+    gateway  => '192.168.1.1',
+    ip_range => '192.168.1.17/32',
+    options  => 'parent=eth0',
   }
 
-  # docker run -d --name pihole \
-  #   -p 53:53/tcp -p 53:53/udp -p 80:80 \
-  #   -v "/home/pi/pihole/etc-pihole:/etc/pihole" \
-  #   -v "/home/pi/pihole/etc-dnsmasq.d:/etc/dnsmasq.d" \
-  #   --dns=127.0.0.1 --dns=1.1.1.1 --hostname pi.hole \
-  #   -e TZ="America/New_York" -e VIRTUAL_HOST="pi.hole" \
-  #   -e PROXY_LOCATION="pi.hole" -e FTLCONF_LOCAL_IPV4="192.168.1.63" \
-  #   pihole/pihole:latest
   docker::run { 'pihole':
     image    => 'pihole/pihole',
     net      => ['pihole'],
